d5ea73bf277db5c3e0fbb128991b6255df0591be
Nancy Ahmed <nancyabdelfattah05@gmail.com>
2025-12-27 14:54:42 +0200

    Refactor CartItem class to improve structure and enhance quantity management

--- models/cart_item.py
+++ models/cart_item.py
@@ -1,29 +1,43 @@
-from extensions import db
+class CartItem:
 
-class CartItem(db.Model):
-  __tablename__ = 'cart_items' 
-  id = db.Column(db.Integer, primary_key=True)
-  user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-  product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
-  _quantity = db.Column('quantity', db.Integer, db.CheckConstraint('quantity > 0'), default=1)
-  product = db.relationship("Product")
+    def __init__(self, item_id, user, product, quantity=1):
+        self._item_id = item_id
+        self._user = user
+        self._product = product
+        self.quantity = quantity
 
-  def __init__(self, user_id, product_id, quantity=1):
-    self.user_id = user_id
-    self.product_id = product_id
-    self._quantity = quantity
+    @property
+    def product(self):
+        return self._product
 
-  @property
-  def quantity(self):
-    return self._quantity
+    @property
+    def quantity(self):
+        return self._quantity
 
-  def increase_quantity(self):
-    self._quantity += 1
+    @quantity.setter
+    def quantity(self, value):
+        if not isinstance(value, int) or value < 1:
+            raise ValueError("Quantity must be an integer and at least 1")
+        self._quantity = value
 
-  def decrease_quantity(self):
-    if self._quantity > 1:
-      self._quantity -= 1
+    def increase(self):
+        self.quantity += 1
 
-  def item_total_price(self):
-    return self.product.price * self._quantity
-    
+    def decrease(self):
+        self.quantity -= 1
+
+    @property
+    def item_total_price(self):
+        return self._product.price * self.quantity
+
+    def to_dict(self):
+        return {
+            "item_id": self._item_id,
+            "user_id": self._user.id,
+            "product_name": self._product.name,
+            "quantity": self.quantity,
+            "item_total_price": self.item_total_price
+        }
+
+    def __repr__(self):
+        return f"<CartItem ID {self._item_id}: {self._product.name} (Qty: {self.quantity})>"
