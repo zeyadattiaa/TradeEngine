932cd64db95ea013b101b25db3bfd293fff784b6
Nancy Ahmed <nancyabdelfattah05@gmail.com>
2025-12-28 19:07:53 +0200

    register cart and wishlist blueprints in app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask
 from Database.db_manager import init_schema
 from routes.auth_routes import auth_bp
 from routes.product_route import shop_bp
-
+from routes.cart_routes import cart_bp
+from routes.wishlist_routes import wishlist_bp
 
 app = Flask(__name__)
 
@@ -14,6 +15,8 @@ init_schema()
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(shop_bp)
+app.register_blueprint(cart_bp)
+app.register_blueprint(wishlist_bp)
 
 
 
